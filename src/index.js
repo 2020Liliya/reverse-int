e990355ae0ae182fe02ea51a661a02bc6a4a86b2
Lily Trofimova <72882836+2020Liliya@users.noreply.github.com>
2022-07-10 22:11:36 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let moduleNumber = String(Math.abs(n));
+    let arrayNumber = moduleNumber.split('');
+    arrayNumber.reverse();
+    const newNumber = Number(arrayNumber.join(''));
+    return newNumber;
 }
